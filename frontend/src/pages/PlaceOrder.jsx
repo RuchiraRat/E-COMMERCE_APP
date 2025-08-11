c244827466e94ccc76cd15f7fea55fb444b813b8
Ruchira Rathnayake <160230053+RuchiraRat@users.noreply.github.com>
2025-08-11 17:02:16 +0530

    Add order listing and payment method selection
    Enhanced Orders page to display a list of products from context. Updated PlaceOrder page to support payment method selection (Master, Visa, Cash on Delivery) and navigation to orders after placing an order.

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import Title from '../components/Title'
 import CartTotal from '../components/CartTotal'
 import { assets } from '../assets/assets'
+import { ShopContext } from '../context/ShopContext'
 
 const PlaceOrder = () => {
+
+  const [method,setMethod] = useState('cod');
+
+  const {navigate} = useContext(ShopContext);
+
   return (
     <div className='flex flex-col sm:flex-row justify-between gap-8 pt-5 sm:pt-14 min-h-[80vh] border-t'>
 
@@ -72,11 +78,23 @@ const PlaceOrder = () => {
         {/* ---------------------Payment Method Selection--------------------- */}
 
         <div className='flex gap-3 flex-col lg:flex-row'>
-          <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
-            <p className={`min-w-3.5 h-3.5 border rounded-full `}></p>
+          <div onClick={()=>setMethod('master')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+            <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'master' ? 'bg-green-400' : ''}`}></p>
             <img className='h-5 mx-4' src={assets.master_logo} alt='' />
           </div>
+          <div onClick={()=>setMethod('visa')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+            <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'visa' ? 'bg-green-400' : ''}`}></p>
+            <img className='h-5 mx-4' src={assets.visa_logo} alt='' />
+          </div>
+          <div onClick={()=>setMethod('cod')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+            <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'cod' ? 'bg-green-400' : ''}`}></p>
+            <p className='text-gray-500 text-sm font-medium mx-4'>CASH ON DELIVERY</p>
+          </div>
+        </div>
 
+
+        <div className='w-full text-end mt-8'>
+          <button onClick={()=>navigate('/orders')} className='bg-black text-white px-16 py-3 text-sm'>PLACE ORDER</button>
         </div>
       </div>
     </div>
